ea7f2c2a3b9420ddf9ea8df394a2b7fa4592d367
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-10-20 16:43:54 +0100

    Added 3 Movies into Database

---
+++ ASP-Razor-Movies/Data/Migrations/20211020145404_InitialCommit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP_Razor_Movies.Data.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gentre",
+                table: "Movie",
+                newName: "Genre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Movie",
+                newName: "Gentre");
+        }
+    }
+}
